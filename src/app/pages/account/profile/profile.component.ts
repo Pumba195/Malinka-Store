992ae595a4a1176f96106a7edce8a033e74dadc0
Maksym <stud_pm22_11@zfkkt.org.ua>
2026-04-02 21:24:32 +0200

    Used Control-flow

--- src/app/pages/account/profile/profile.component.ts
+++ src/app/pages/account/profile/profile.component.ts
@@ -2,11 +2,11 @@ import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
-import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-profile',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
